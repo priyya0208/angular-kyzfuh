33f740c940c1fe4aa59e43218423da52d0e339d8
priyya0208 <priyya.0208@gmail.com>
2020-01-15 13:25:10 +0530

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, ServerComponent ],
+  imports:      [ BrowserModule, FormsModule  ],
+  declarations: [ AppComponent, ServerComponent, WarningAlertComponent, SuccessAlertComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector : 'app-server',
-  templateUrl : './server.Component.html'
+  templateUrl : 'server.component.html'
 })
 
-export class ServerComponent {
-  
+export class ServerComponent  implements OnInit {
+
+  constructor() {
+    setTimeout(() => {this.allowNewServer = true }, 2000)
+   }
+  serverId:number = 10;
+  serverStatus: string = "Offline";
+  serverEventClicked = "No server Clicked";
+  allowNewServer = false;
+  serverName="testServer";
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  onServerClicked() {
+    return this.serverEventClicked = "Server Clicked";
+  }
+  onUpdateServerName(event: Event) {
+    return this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  ngOnInit() {
+  }
 }
