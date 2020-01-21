c04964085b1e800a7c7a5778318c10dc8aefc64e
priyya0208 <priyya.0208@gmail.com>
2020-01-21 16:14:14 +0530

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule  ],
-  declarations: [ AppComponent, ServerComponent, WarningAlertComponent, SuccessAlertComponent ],
+  declarations: [ AppComponent, ServerComponent, WarningAlertComponent, SuccessAlertComponent, CockpitComponent, ServerElementComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
